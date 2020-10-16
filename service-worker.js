afe30810500949328349361ab421c50850056edb
Ameya Pandit <ameyapandit@Ameyas-MacBook-Pro.local>
2020-10-16 05:59:06 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3a49737b34ad19055c8eade5f8a5671.js"
+  "/precache-manifest.3399601a698e32a0c5d089ccaa1d4a10.js"
 );
 
 self.addEventListener('message', (event) => {
